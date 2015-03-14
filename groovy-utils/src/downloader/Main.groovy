43389328321dd88593c47aa4f11b4668f746b669
slavek <Slvek@slwnb>
2015-03-14 22:04:51 +0100

    A new format of input strings introduced.

--- groovy-utils/src/downloader/Main.groovy
+++ groovy-utils/src/downloader/Main.groovy
@@ -5,8 +5,9 @@ final WORKING_DIR = "f:\\PODCASTS\\";
 final String DEFINITIONS_FILE = "definitions.txt";
 
 // Replacements
-final String REPLACE_SOURCE = "www2.rozhlas.cz/stream";
-final String REPLACE_TARGET = "media.rozhlas.cz/_audio";
+// Supported format examples: prehravac.rozhlas.cz/audio/3304377
+def KNOWN_PATTERNS = [ ~/prehravac.rozhlas.cz\/audio\/([0-9]+)$/ ]
+def TARGET_PATTERN = "http://media.rozhlas.cz/_audio/<identifier>.mp3"
 
 
 // Implementation begin
@@ -25,6 +26,17 @@ if (definitions.exists()) {
 		
 		print "Processing ${line}"
 		
+		for (def pattern : KNOWN_PATTERNS) {
+			def matcher = line =~  pattern
+			
+			// If current line matches a known pattern
+			if (matcher.size() > 0) {
+				def identifier = matcher[0][1]
+				line = TARGET_PATTERN.replace("<identifier>", identifier);
+				break
+			}
+		}
+		
 		String target = line.substring(line.lastIndexOf('/') + 1);
 		def targetFile = new File(WORKING_DIR + target);
 		
@@ -32,7 +44,6 @@ if (definitions.exists()) {
 			targetFile.delete();
 		}
 		
-		line = line.replaceFirst(REPLACE_SOURCE, REPLACE_TARGET);
 		targetFile << line.toURL().openStream();
 		
 		println " ... downloaded to ${targetFile}"
